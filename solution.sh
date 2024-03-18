2666081e0507216eea55bfc1cd1cf8f212e3220e
Karawan Sh. Y <karawan.sh.y@gmail.com>
2024-03-18 11:37:01 +0200

    Implement first 3 options in the menu

--- solution.sh
+++ solution.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
 
+display_in_order(){
+	echo "The list in the order it was entered: ${series[@]}"
+}
+
+display_sorted(){
+	local sorted=($(for number in "${series[@]}"; do 
+						echo "$number"; 
+						done | sort -n))
+	
+	echo "The sorted list is: ${sorted[*]}"
+}
