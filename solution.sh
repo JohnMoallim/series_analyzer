00a726fbde60f9d487de3e7c306b0253367ba96d
karam.aamar <karam.sa987@gmail.com>
2024-03-18 11:43:23 +0200

    second commit

--- solution.sh
+++ solution.sh
@@ -16,3 +16,38 @@ input_series()
     echo "${series[@]}"
 }
 
+main() {
+    if [ $# -ge 3 ]; then
+        series=("$@")
+    else
+        series=$(input_series)
+    fi
+
+    while true; do
+        echo "working menu"
+        echo "a. Input a Series"
+        echo "b. Display the series in the order it was entered"
+        echo "c. Display the series in sorted order"
+        echo "d. Display the Max value of the series"
+        echo "e. Display the Min value of the series"
+        echo "f. Display the Average of the series"
+        echo "g. Display the Number of elements in the series"
+        echo "h. Display the Sum of the series"
+        echo "i. Exit"
+
+        read -r choice
+        case "$choice" in
+            a) series=$(input_series);;
+            b) display_series;;
+            c) display_sorted_series;;
+            d) display_max;;
+            e) display_min;;
+            f) display_average;;
+            g) display_count;;
+            h) display_sum;;
+            i) exit;;
+            *) echo "invalid choice";;
+        #end of case
+        esac
+    done
+}
