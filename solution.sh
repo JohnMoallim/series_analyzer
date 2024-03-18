f51b0deaebf99f5d74e52f56b6969f0b13a24254
karam.aamar <karam.sa987@gmail.com>
2024-03-18 11:28:11 +0200

    first commit

--- solution.sh
+++ solution.sh
@@ -1,2 +1,18 @@
 #!/bin/bash
 
+input_series() 
+{
+    local series=()
+    local num
+    echo "enter a positive number or 'done' to finish"
+    
+    while read -r num && [[ "$num" != "done" ]]; do
+        if [[ "$num" =~ ^[0-9]+$ && "$num" -gt 0 ]]; then
+            series+=("$num")
+        else
+            echo "invalid input"
+        fi
+    done
+    echo "${series[@]}"
+}
+
